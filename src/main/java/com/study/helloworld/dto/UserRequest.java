c9747515bac801b9178c0c03e1502431c5b002ca
highkim0922 <shineest96@gmail.com>
2022-03-24 14:33:37 +0900

    Add UserRequestDto

---
+++ src/main/java/com/study/helloworld/dto/UserRequest.java
@@ -0,0 +1,40 @@
+package com.study.helloworld.dto;
+
+public class UserRequest {
+    private String name;
+    private String email;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
